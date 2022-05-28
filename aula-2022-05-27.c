e2a98a5ab2f9f7bac4726424d5b9ac4eeb2b1062
mtolezano <106412029+mtolezano@users.noreply.github.com>
2022-05-27 22:02:55 -0300

    Add files via upload

---
+++ aula-2022-05-27.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main () {
+printf("Hello world\n");
+printf("%25d\n", 22);
+printf("%05.2f\n", 7.14);
+printf("%.2f\n", 27.2);
+printf("%30s\n", "Consegui a IC!");
+printf("%010d\n", 52);
+printf("Nome: %s \n Idade: %d \n ", "Matheus", 25);
+
+
+
+
+}
+
+
+
