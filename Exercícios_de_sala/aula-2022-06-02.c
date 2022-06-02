e3c1e7682891efb528cd61687f584d37489dbe8d
mtolezano <106412029+mtolezano@users.noreply.github.com>
2022-06-02 19:49:16 -0300

    Add files via upload

---
+++ Exercícios_de_sala/aula-2022-06-02.c
@@ -0,0 +1,44 @@
+/*Matrícula: 2021101302 
+Ler um inteiro n1
+Ler um inteiro n2
+Multiplicar n1 com n2
+Resultado = n1*n2
+imprimir resultado
+A variável utilizada será no formato float para ler números reais
+*/
+ 
+#include <stdio.h>
+int main () {
+
+float seg, ter, quar, quin, sex, soma=0, desp1, desp2, desp3, desp4, desp5, saida, entrada;
+printf("Informe o valor de entrada da segunda-feira:\n");
+scanf("%f", &seg);
+printf("Informe o valor de despesas da segunda-feira:\n");
+scanf("%f", &desp1);
+soma += seg + desp1;
+if (soma
+printf("Informe o valor de entrada da terça-feira:\n");
+scanf("%f", &ter);
+printf("Informe o valor de despesas da terça-feira:\n");
+scanf("%f", &desp2);
+printf("Informe o valor de entrada da quarta-feira:\n");
+scanf("%f", &quar);
+printf("Informe o valor de despesas da quarta-feira:\n");
+scanf("%f", &desp3);
+printf("Informe o valor de entrada da quinta-feira:\n");
+scanf("%f", &quin);
+printf("Informe o valor de despesas da quinta-feira:\n");
+scanf("%f", &desp4);
+printf("Informe o valor de entrada da sexta-feira:\n");
+scanf("%f", &sex);
+printf("Informe o valor de despesas da sexta-feira:\n");
+scanf("%f", &desp5);
+printf("Informe o valor de entrada da semana:\n");
+scanf("%f", &entrada);
+printf("Informe o valor de saída da semana:\n");
+scanf("%f", &saida);
+
+
+
+}
+
