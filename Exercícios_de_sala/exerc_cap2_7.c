3db51ae628bab149539d9c10baa8649b6e6da94a
mtolezano <106412029+mtolezano@users.noreply.github.com>
2022-06-14 13:53:59 -0300

    Add files via upload

---
+++ Exercícios_de_sala/exerc_cap2_7.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+int main ()
+{ 
+  int numero;
+printf("Informe o número:");
+scanf("%d", &numero);
+
+  if (numero >= 20 && numero <= 50)
+printf("Este número está entre 20 e 50");
+  else 
+printf("Este número não está entre 20 e 50");
+
+}	  
