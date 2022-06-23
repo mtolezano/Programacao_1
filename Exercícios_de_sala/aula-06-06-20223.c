d3101630bae914f7f86179fbad0d4dfe57b6e4c6
mtolezano <106412029+mtolezano@users.noreply.github.com>
2022-06-23 19:55:40 -0300

    Add files via upload

---
+++ Exercícios_de_sala/aula-06-06-20223.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main () {
+	int i;
+	printf("Números divisíveis por 3 que são pares:\n");
+	i = 0;
+	while (i<=40)
+	{
+	        i+=2;
+		if (i % 3 == 0)
+			printf("%d\n", i);
+		i++6;
+	}		
+}	
+	
