97dac6fad439763d726109cff18337697f5f6bc2
mtolezano <106412029+mtolezano@users.noreply.github.com>
2022-07-08 20:40:50 -0300

    Add files via upload

---
+++ Exercícios_de_sala/aula-2022-07-08.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main () {
+	float nota[5];
+	int i;
+	for (i=0; i<5; i++) { 
+		printf("Digite a nota do aluno %d: ", i);
+		scanf("%f", &nota[i]);
+			    }
+	for (i=0; i<5; i++) {
+		printf("Nota do aluno %d: %.2f \n", i, nota[i]);
+			    }	    
+	    }		    
+			    
+			    
