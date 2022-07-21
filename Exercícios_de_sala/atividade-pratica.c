d7566f982afb43c786565600e4c893dcde0a8fe3
mtolezano <106412029+mtolezano@users.noreply.github.com>
2022-07-21 18:31:44 -0300

    Add files via upload

---
+++ Exercícios_de_sala/atividade-pratica.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main () { 
+	int mat[10][10], lin, col;
+	   for (lin = 0; lin < 10; lin++) { 
+	     for (col = 0; col < 10; col++) { 
+	        mat[lin][col] = rand()%1000;
+	        printf("%d ", mat[lin][col]);
+	      }
+	   printf("\n");   
+	    }
+	 }   
+	    
+	    
+	    
+	      
+	
