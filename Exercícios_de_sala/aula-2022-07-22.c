d08e43b4175e322efc9bb9f2e002d1f6e822696a
mtolezano <106412029+mtolezano@users.noreply.github.com>
2022-07-22 20:51:46 -0300

    Update aula-2022-07-22.c

--- Exercícios_de_sala/aula-2022-07-22.c
+++ Exercícios_de_sala/aula-2022-07-22.c
@@ -1,3 +1,4 @@
+//exercício 1:
 #include <stdio.h> 
   typedef struct {
      char nome[20];
@@ -12,7 +13,23 @@ int main ()
      printf("Digite sua idade:");
      scanf("%d", &var1.idade);
      printf("Seu nome é: %s\n Sua idade é: %d\n", var1.nome, var1.idade);
-}     
+}
+
+// exercício 2:
+#include <stdio.h> 
+typedef struct { 
+   char nome[20];
+   int idade;
+   float nota;
+   }tipo_aluno;
+int main () { 
+   tipo_aluno aluno;
+   scanf("%s %d %f", aluno.nome, &aluno.idade, &aluno.nota);
+   printf("Seu nome é: %s\n Sua idade é: %d anos\n Sua nota é: %.2f\n", aluno.nome, aluno.idade, aluno.nota);
+   }
+
+
+
      
      
          
