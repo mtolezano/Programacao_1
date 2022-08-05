8d7f514ac907acc855f9731457638cf496c9f3f8
mtolezano <106412029+mtolezano@users.noreply.github.com>
2022-08-05 20:30:34 -0300

    Update aula2022-05-08.c

--- Exercícios_de_sala/aula2022-05-08.c
+++ Exercícios_de_sala/aula2022-05-08.c
@@ -1,3 +1,4 @@
+//exercício 1:
 #include <stdio.h> 
 #include <stdlib.h>
 typedef struct { 
@@ -9,4 +10,29 @@ int main () {
     scanf("%f", &imc -> peso);
     printf("%f\n", imc -> peso);
     
+    }  
+
+//exercício 2:
+#include <stdio.h> 
+#include <stdlib.h>
+#include <math.h>
+typedef struct { 
+    float peso, altura;
+    } tipoIMC;
+int main () { 
+    tipoIMC *imc;
+    imc = malloc (sizeof(tipoIMC));
+    //scanf("%f", &imc -> peso);
+    //printf("%f\n", imc -> peso);
+    printf("Insira o peso.:");
+    scanf("%f", &imc -> peso);
+    
+    printf("Insira sua altura.:");
+    scanf("%f", &imc -> altura);
+    
+    printf("Seu peso é.: %.2f\nSua altura é.: %.2f\n", imc -> peso, imc -> altura);
+    
+    printf("Seu imc é.:%f\n", imc -> peso/ pow (imc -> altura, 2));
+    
     }    
+//exercício 3:
