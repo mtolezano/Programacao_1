7c41e114f87789584102c2627f38a93260694ea6
mtolezano <106412029+mtolezano@users.noreply.github.com>
2022-08-11 19:46:31 -0300

    Update aula2022-08-11.c

--- Exercícios_de_sala/aula2022-08-11.c
+++ Exercícios_de_sala/aula2022-08-11.c
@@ -13,3 +13,45 @@ int main ()
 }
 
 //exercício 2:
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int p_impar (int vet [5], int pos){ 
+	if (pos >= 0 && pos < 5){
+	    if (vet[pos] % 2 == 1){
+	        return 1;
+	    } else { 
+	        return 0;
+	    }
+	}
+	    }	
+	  
+int main () 
+{ 
+    int v [5] = {2, 4, 6, 1, 7}, resp;    
+    resp = p_impar(v,0);
+    printf("%s\n", (resp == 1)? "sim":"nao"); 
+} 
+
+//exercício 2 (part2):
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int p_impar (int vet [5], int pos){ 
+	if (pos >= 0 && pos < 5){
+	    if (vet[pos] % 2 == 1){
+	        return 1;
+	    } else { 
+	        return p_impar (vet, pos + 1);
+	    }
+	}
+	    }	
+	  
+int main () 
+{ 
+    int v [5] = {2, 4, 6, 1, 7}, resp;    
+    resp = p_impar(v,0);
+    printf("%s\n", (resp == 1)? "sim":"nao"); 
+}   
+                   
+                   
